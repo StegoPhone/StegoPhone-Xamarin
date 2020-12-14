7086f20b24fb422278da2473e14417ec3c3497f4
Jessica Mulein <jessica@mulein.com>
2020-12-14 01:56:22 -0800

    New project

---
+++ StegoPhone-Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StegoPhone_Xamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StegoPhone_Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
